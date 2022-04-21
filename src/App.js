64f6ae3a64c18ea515b6776b4b7c9fabac3465e8
mafqla <1692285748@qq.com>
2022-04-21 18:17:00 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// 导入路由
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
+// 导入页面组件
+import Login from './pages/Login'
+import Layout from './pages/Layout'
+
+// 配置路由规则
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<Layout/>}/>
+                    <Route path="/login" element={<Login/>}/>
+                </Routes>
+            </div>
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
---
+++ src/pages/Login/index.js
@@ -0,0 +1,14 @@
+import { Card } from 'antd'
+import './index.scss'
+
+const Login = () => {
+    return (
+        <div className="login">
+            <Card className="login-container">
+
+            </Card>
+        </div>
+    )
+}
+
+export default Login
